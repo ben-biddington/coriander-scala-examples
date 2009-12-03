92f11978a2c3fbd76d9f7fdae59aebd9680738fb
Ben Biddington <ben.biddington@gmail.com>
2009-12-03 20:50:06 +0000

    Added eta expansion/closure examples

--- test/org/coriander/learning/scala/functions/examples/EtaExpansion.scala
+++ test/org/coriander/learning/scala/functions/examples/EtaExpansion.scala
@@ -10,8 +10,26 @@ import org.coriander.learning.scala.TestBase
 class EtaExpansion extends TestBase {
 	@Test
 	def methods_may_be_used_as_functions_by_automatic_eta_expansion {
+		val availableToEtaExpandedClosure = 10
+
 		val cubed : Int = cubeByLambda(2, cubeAsEta)
+
 		assertThat(cubed, is(equalTo(8)))			
+	}
+
+	@Test
+	def methods_may_be_used_as_closures {
+		def cubeByClosure = getCubeClosure
+
+		val cubed = cubeByClosure(2)
+
+		assertThat(cubed, is(equalTo(80)))
+	}
+
+	def getCubeClosure : Int => Int = {
+		val multiplier = 10
+
+		in => (in * in * in) * multiplier
 	}
 
 	def cubeAsEta(in : Int) : Int = in * in * in
