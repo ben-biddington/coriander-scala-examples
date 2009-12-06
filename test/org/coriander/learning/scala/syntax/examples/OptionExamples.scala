39911faa3b74d83c5dbacada35fc5a2aa266279a
Ben Biddington <ben.biddington@gmail.com>
2009-12-06 13:21:34 +0000

    Added more Option examples

--- test/org/coriander/learning/scala/syntax/examples/OptionExamples.scala
+++ test/org/coriander/learning/scala/syntax/examples/OptionExamples.scala
@@ -7,17 +7,28 @@ import org.hamcrest.core.IsNot._
 import org.hamcrest.core.IsEqual._
 
 class OptionExamples {
-    val simpleData = Map("a" -> "Apple", "b" -> "Ball")
-	
-	@Test def missing_data_returns_none {
-		val missingData = simpleData get ("c")
+    val simpleData 					= Map("a" -> "Apple", "b" -> "Ball")
+	val A_KEY_THAT_DOES_NOT_EXIST 	= "c"
+	val A_KEY_THAT_EXISTS 			= "a"
 
-		assertTrue(missingData == None)
+	@Test def missing_data_returns_none_and_is_empty {
+		val missingData : Option[String] = simpleData get (A_KEY_THAT_DOES_NOT_EXIST)
+
+		assertTrue(missingData == None)		
+		assertTrue(missingData isEmpty)
+	}
+
+	@Test { val expected = classOf[NoSuchElementException] }
+	def invoking_get_on_none_throws_exception {
+		val missingData : Option[String] = simpleData get (A_KEY_THAT_DOES_NOT_EXIST)
+
+		val theValue = missingData get;
 	}
 
 	@Test def successful_search_returns_some {
 		val someData : Option[String] = simpleData get("a")
 
 		assertThat(someData.get, is(equalTo("Apple")))
+		assertFalse(someData isEmpty)
 	}
 }
