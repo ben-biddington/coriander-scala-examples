4a33008ebffa558abce61c16e980d0ce7d4835e3
Ben Biddington <ben.biddington@gmail.com>
2009-12-15 15:08:24 +0000

    Concurrent sort example

--- test/org/coriander/learning/scala/concurrency/examples/ActorExamples.scala
+++ test/org/coriander/learning/scala/concurrency/examples/ActorExamples.scala
@@ -1,14 +1,16 @@
 package org.coriander.learning.scala.concurrency.examples
 
 import _root_.scala.actors.Actor._
-import org.junit.Test
 import org.junit.Assert._
 import org.hamcrest.core.Is._
 import org.hamcrest.core.IsNot._
 import org.hamcrest.core.IsEqual._
 import java.lang.Thread._
+import actors.Actor
+import org.junit.{Test}
+import collection.mutable.ListBuffer
 
-class ActorExamples {
+class ActorExamples {	
 	@Test
 	def bang_query_blocks_caller_until_actor_returns {
 		val anActorThatReturnsItsThreadId = actor {
@@ -19,17 +21,65 @@ class ActorExamples {
 			}
 		}
 
-		var actualThreadUsed : String = null
-
 		anActorThatReturnsItsThreadId !? "xxx" match {
-			case reply : String => actualThreadUsed = reply
+			case reply : String => {
+				val actualThreadUsedByChildActor = reply
+				assertThat(currentThreadId, is(not(equalTo(actualThreadUsedByChildActor))))
+			}
+		}
+	}
+
+	@Test
+	def parent_can_block_until_actors_all_complete {
+		var result 		= new ListBuffer[Int]
+		var sortedCount = 0
+
+		newParallelSorter ! randomIntegers(100)
+		newParallelSorter ! randomIntegers(100)
+		newParallelSorter ! randomIntegers(100)
+
+		while (sortedCount < 3) {
+		  	receive {
+				case list : List[Int] => {
+					result.appendAll(list)
+					sortedCount += 1
+				}
+			}
 		}
 
-		assertThat(currentThreadId, is(not(equalTo(actualThreadUsed))))
+		val sortedList = result.toList
+
+		console("Sort complete, and full list contains " + sortedList.size + " items")
+
+		assert(sortedList.first < sortedList.last)
+		assertThat(sortedList.length, is(equalTo(300)))
+	}
+
+	private def randomIntegers(howMany : Int) = {
+		val random = new java.util.Random()
+		var result = new ListBuffer[Int]
+
+		for (val i <- 0 until howMany) result += random.nextInt
+
+		result.toList
 	}
 
 	private def console(message : String) {
 		println("[" + currentThreadId + "] " + message)
+	}
+
+	val newParallelSorter : Actor = actor {
+		loop {
+			receive {
+				case list : List[Int] => {
+					reply(sort(list))
+				}
+			}
+		}
+	}
+
+	private def sort(list : List[Int]) : List[Int] = {
+		list sort(_<_)
 	}
 
 	private def currentThreadId = currentThread.getId.toString
