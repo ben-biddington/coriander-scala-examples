6c42f4f608342bcf8637b596dccc39f6060531a2
Ben Biddington <ben.biddington@gmail.com>
2009-12-15 09:26:42 +0000

    Added actor bang-query example

--- test/org/coriander/learning/scala/concurrency/examples/ActorExamples.scala
+++ test/org/coriander/learning/scala/concurrency/examples/ActorExamples.scala
@@ -7,38 +7,30 @@ import org.hamcrest.core.Is._
 import org.hamcrest.core.IsNot._
 import org.hamcrest.core.IsEqual._
 import java.lang.Thread._
-import collection.mutable.ArrayBuffer
 
 class ActorExamples {
-    @Test
-	def each_actor_represents_a_thread {
-		println("[" + currentThread.getId.toString + "] Starting...")
-		startProcess(List(1, 2, 3, 4, 5))
-		startProcess(List(1, 2, 3, 4, 5))
-		startProcess(List(6, 7, 8, 9, 10))
-		startProcess(List(6, 7, 8, 9, 10))
-
-		Thread.sleep(2000)
-
-		println("[" + currentThread.getId.toString + "] Done ")
-    }
-
-	private def startProcess(list : Iterable[Int]) {
-		val newThread = actor {
+	@Test
+	def bang_query_blocks_caller_until_actor_returns {
+		val anActorThatReturnsItsThreadId = actor {
 			loop {
 				receive {
-					case list : List[Int] => {
-						Thread.sleep(1000)
-						self ! done ("[" + currentThread.getId.toString + "] Received: " + list)
-					}
+					case _ => reply(currentThreadId)
 				}
 			}
 		}
 
-		newThread ! list
+		var actualThreadUsed : String = null
+
+		anActorThatReturnsItsThreadId !? "xxx" match {
+			case reply : String => actualThreadUsed = reply
+		}
+
+		assertThat(currentThreadId, is(not(equalTo(actualThreadUsed))))
 	}
 
-	private def done(message : String) {
-		println(message)
+	private def console(message : String) {
+		println("[" + currentThreadId + "] " + message)
 	}
+
+	private def currentThreadId = currentThread.getId.toString
 }
